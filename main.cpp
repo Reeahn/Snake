87cac83cdc44d986a8f0e43df5122f7def24e9e2
Reeahn <36976247+Reeahn@users.noreply.github.com>
2021-03-09 07:22:56 +0930

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,9 @@ enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
 eDirection dir;
 const int maxBody = (width - 1) * (height - 1);
 int bodyX[maxBody];
+
 int bodyY[maxBody];
+bool bodyCreated = false;
 
 void Setup()
 {
@@ -20,19 +22,17 @@ void Setup()
 	x = width / 2;
 	y = height / 2;
 	bodyLength = 0;
-	for (int i = 0; i<maxBody;i++) {
-		if (i!=0) {
-			bodyX[i] = NULL;
-			bodyY[i] = NULL;
-		}
-		else {
-			bodyX[i] = x;
-			bodyY[i] = y;
-		}
-	}
-	fruitX = rand() % (width-1)+1;
-	fruitY = rand() % (height-1)+1;
+	bodyX[0] = x+1;
+	bodyY[0] = y;
+	fruitX = rand() % (width - 1) + 1;
+	fruitY = rand() % (height - 1) + 1;
 	score = 0;
+}
+
+void cls() {
+	static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+
 }
 
 void Draw()
@@ -46,6 +46,16 @@ void Draw()
 	{
 		for (int j = 0; j < width; j++)
 		{
+			bodyCreated = false;
+			for (int k = 0; k < maxBody; k++) {
+				if (bodyX[k] == NULL) {
+					break;
+				}
+				else if (bodyX[k] == j && bodyY[k] == i) {
+					std::cout << "S";
+					bodyCreated = true;
+				}
+			}
 			if (j == 0) {
 				std::cout << "#";
 			}
@@ -55,16 +65,8 @@ void Draw()
 			else if (i == fruitY && j == fruitX) {
 				std::cout << "F";
 			}
-			else {
-				for (int k = 0; k < maxBody; k++) {
-					if (bodyX[k] == NULL) {
-						break;
-					}
-					else if (bodyX[k] == j && bodyY[k] == i) {
-						std::cout << "S";
-					}
-				}
-				std::cout << " ";
+			else if (bodyCreated != true) {
+					std::cout << " ";			
 			}
 			if (j == width - 1)
 				std::cout << "#" << std::endl;
@@ -72,10 +74,6 @@ void Draw()
 	}
 	for (int i = 0; i < width + 1; i++)
 		std::cout << "#";
-	std::cout << std::endl;
-	std::cout << fruitX;
-	std::cout << std::endl;
-	std::cout << fruitY;
 }
 void Input()
 {
@@ -89,54 +87,98 @@ void Input()
 	int D = GetAsyncKeyState(0x44);
 
 	if (W != 0) {
-		dir = UP;
-	} else if (A != 0) {
-		dir = LEFT;
-	} else if (S != 0) {
-		dir = DOWN;
-	} else if (D != 0) {
-		dir = RIGHT;
+		if (dir == DOWN) {
+			dir = DOWN;
+		}
+		else {
+			dir = UP;
+		}
+	}
+	else if (A != 0) {
+		if (dir == RIGHT) {
+			dir = RIGHT;
+		}
+		else {
+			dir = LEFT;
+		}
+	}
+	else if (S != 0) {
+		if (dir == UP) {
+			dir = UP;
+		}
+		else {
+			dir = DOWN;
+		}
+	}
+	else if (D != 0) {
+		if (dir == LEFT) {
+			dir = LEFT;
+		}
+		else {
+			dir = RIGHT;
+		}
 	}
 
 }
-void UpdateBody() 
+void UpdateBody()
 {
 	// Update the position of each body to its predecessors 
 	// position creating a tail. Stopping the update process
 	// if we reach the end of the tail.
+
+	int tempX;
+	int tempXX;
+	int tempY;
+	int tempYY;
+
 	for (int i = 0; i < maxBody; i++) {
 		if (bodyX[i] == NULL) {
 			break;
 		}
 		else if (i == 0) {
+			tempX = bodyX[i];
+			tempY = bodyY[i];
 			bodyX[i] = x;
 			bodyY[i] = y;
 		}
 		else {
-			bodyX[i] = bodyX[i - 1];
-			bodyY[i] = bodyY[i - 1];
+			tempXX = bodyX[i];
+			bodyX[i] = tempX;
+			tempYY = bodyY[i];
+			bodyY[i] = tempY;
+			tempX = tempXX;
+			tempY = tempYY;
 		}
 	}
 }
 void Logic()
 {
-	switch (dir) 
+	if (x == fruitX && y == fruitY)
 	{
-		case 3:
-			UpdateBody();			
-			y -= 1;
-			break;
-		case 1:
-			UpdateBody();
-			x -= 1;
-			break;
-		case 4:
-			UpdateBody();
-			y += 1;
-			break;
-		case 2: 
-			UpdateBody();
-			x += 1;
+		fruitX = rand() % (width - 1) + 1;
+		fruitY = rand() % (height - 1) + 1;
+		bodyLength += 1;
+		bodyX[bodyLength] = bodyX[bodyLength - 1];
+		bodyY[bodyLength] = bodyY[bodyLength - 1];
+		score += 1;
+	}
+	switch (dir)
+	{
+	case 3:
+		UpdateBody();
+		y -= 1;
+		break;
+	case 1:
+		UpdateBody();
+		x -= 1;
+		break;
+	case 4:
+		UpdateBody();
+		y += 1;
+		break;
+	case 2:
+		UpdateBody();
+		x += 1;
 	}
 	if (x == 0 || x == width) {
 		gameOver = true;
@@ -144,16 +186,14 @@ void Logic()
 	else if (y == 0 || y == height) {
 		gameOver = true;
 	}
-	if (x == fruitX && y == fruitY)
-	{
-		fruitX = rand() % (width - 1) + 1;
-		fruitY = rand() % (height - 1) + 1;
-		bodyLength += 1;
-		bodyX[bodyLength] = bodyX[bodyLength-1];
-		bodyY[bodyLength] = bodyY[bodyLength-1];
-		score += 1;
+	for (int i = 0; i < maxBody; i++) {
+		if (bodyX[i] == NULL) {
+			break;
+		}
+		else if (x == bodyX[i] && y == bodyY[i]) {
+			gameOver = true;
+		}
 	}
-
 }
 int main()
 {
@@ -164,8 +204,6 @@ int main()
 		Sleep(100);
 		Input();
 		Logic();
-		
-
 	}
 	return 0;
 }
